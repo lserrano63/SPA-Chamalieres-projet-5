5dcc8e449ad9ad01f2d15f03b6a26f776aa45027
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-04-20 17:34:02 +0200

    Dossier de base

--- Models/Manager.php
+++ Models/Manager.php
@@ -1,17 +1,1 @@
 <?php
-
-class Manager {
-
-    protected function dbConnection()
-    {
-        try
-        {
-            $db = new PDO('mysql:host=db5000352654.hosting-data.io;port=3306;dbname=dbs343143;charset=utf8', 'dbu620826', 'kjbqs;hbdn3.@S');
-            return $db;
-        }
-        catch(Exception $e)
-        {
-            die('Erreur : '.$e->getMessage());
-        }
-    }
-}
--- Views/template.php
+++ Views/template.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" type="image/png" href="images/favicon.jpg">
+        <link rel="shortcut icon" type="image/png" href="">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/f42ba57ba1.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -15,29 +15,6 @@
     </head>
         
     <body>
-        <header>
-            <nav class="d-flex justify-content-center text-center">
-                <ul class="container nav row">
-                    <div class="col-12 d-flex flex-wrap">
-                        <?php 
-                            if (isset($_SESSION['connected']) && $_SESSION['connected'] == true) {
-                            ?>
-                            <li class="nav-item col-sm-3 col-6"><a class="nav-link" href="index.php">Accueil</a></li>
-                            <li class="nav-item col-sm-3 col-6"><a class="nav-link" href="?action=propos">A propos</a></li>
-                            <li class="nav-item col-sm-3 col-6"><a class="nav-link" href="?action=admin">Administration</a></li>
-                            <li class="nav-item col-sm-3 col-6"><a class="nav-link" href="?action=disconnect">Déconnexion</a></li>
-                            <?php    
-                            } else {
-                            ?>
-                                <li class="nav-item col-sm-6 col-6"><a class="nav-link" href="index.php">Accueil</a></li>
-                                <li class="nav-item col-sm-6 col-6"><a class="nav-link" href="?action=propos">A propos</a></li>
-                        <?php        
-                            }
-                        ?>
-                    </div>   
-                </ul>
-            </nav>
-        </header>
         <?= $content ?>
     </body>
 </html>
