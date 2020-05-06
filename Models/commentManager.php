6dc778d6b820f57096137619bffe33aeeb76dfce
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-05-06 14:16:51 +0200

    commentmanger functions

--- Models/commentManager.php
+++ Models/commentManager.php
@@ -1,1 +1,37 @@
-<?php ?>
+<?php
+require_once('Manager.php');
+class CommentManager extends Manager {
+
+    public function getComments($postId)
+    {
+        $db = $this->dbConnection();
+        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments->execute(array($postId));
+        return $comments;
+    }
+
+    public function addComment($postId, $author, $comment)
+    {
+        $db = $this->dbConnection();
+        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+        $addaComment = $comments->execute(array($postId, $author, $comment));
+        return $addaComment;
+    }
+
+    public function removeComment($comment_id)
+    {
+        $db = $this->dbConnection();
+        $removeaComment = $db->prepare('DELETE FROM comments WHERE reported=1 AND id=?');
+        $remove = $removeaComment->execute(array($comment_id));
+        return $remove;
+    }
+
+    public function removeAllCommentsFromPost($post_id)
+    {
+        $db = $this->dbConnection();
+        $removeaComment = $db->prepare('DELETE FROM comments WHERE post_id=?');
+        $remove = $removeaComment->execute(array($post_id));
+        return $remove;
+    }
+
+}
