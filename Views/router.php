9a7bfd0b98b798c83095ec5fd8b29f5906af2037
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-05-06 14:15:37 +0200

    added some actions

--- Views/router.php
+++ Views/router.php
@@ -6,4 +6,25 @@ if($action == 'spajson')
 {
     require('Models/data_to_json.php');
     require('Views/Private/spa_json.php');
+} elseif($action == 'viewPost')
+{
+    require('Controllers/frontend.php');
+    viewPost();
+} elseif ($action == 'addComment') 
+{
+    if (isset($_GET['id']) && $_GET['id'] > 0) 
+    {
+        if (!empty($_POST['author']) && !empty($_POST['comment'])) 
+        {
+            require('Controllers/frontend.php');
+            addOneComment($_GET['id'], $_POST['author'], $_POST['comment']);
+        }
+    }
+    else {
+        echo 'Erreur : aucun identifiant de billet envoyé';
+    } 
+} elseif ($action=="mentions"){
+    require('Views/mentions.php');
+} elseif ($action=="propos"){
+    require('Views/propos.php');
 }
