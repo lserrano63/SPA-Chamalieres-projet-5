54d48ffbeede12c822d5e0a7d16f17439e6d2990
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-05-09 23:13:08 +0200

    router up for some admin's actions

--- Views/router.php
+++ Views/router.php
@@ -25,6 +25,43 @@ if($action == 'spajson')
     } 
 } elseif ($action=="mentions"){
     require('Views/mentions.php');
-} elseif ($action=="propos"){
-    require('Views/propos.php');
-}
+}  elseif (isset($_SESSION['connected']) && ($_SESSION['connected'] == true)){
+    if ($action == 'disconnect') {
+        $_SESSION = array();
+        session_destroy();
+        header('Location: index.php');
+    } elseif ($action == 'admin') {
+        require('Views/adminView.php');
+    } elseif ($action == 'postCreation') {
+        require('Views/adminPostCreation.php');
+    } elseif ($action == 'addPost') {
+        if (!empty($_POST['title']) && !empty($_POST['post'])) 
+        {
+            require('Controllers/backend.php');
+            addOnePost($_POST['title'], $_POST['post']);
+        }
+    } elseif ($action == 'viewComments') {
+        require('Views/adminManageComments.php');
+    } elseif ($action == 'removeComment') {
+        if (isset($_GET['id']) && $_GET['id'] > 0){
+            require('Controllers/backend.php');
+            removeComment($_GET['id']);
+            header('Location: index.php?action=adminManageComments');
+        }
+    }
+    elseif ($action == 'adminPost') {
+        require('Views/adminPostView.php');
+    } elseif ((isset($_GET['id']) && $_GET['id'] > 0) && ($action == 'adminPostModify')) {
+        require('Controllers/frontend.php');
+        require('Controllers/backend.php');
+        viewPostAdmin();
+    } elseif ((isset($_GET['id']) && $_GET['id'] > 0) && ($action == 'adminPostModified')){
+        require('Controllers/backend.php');
+        modifyOnePost($_POST['title'], $_POST['post'], $_GET['id']);
+        header('Location: index.php?action=adminPost');
+    } elseif ((isset($_GET['id']) && $_GET['id'] > 0) && ($action == 'adminPostDelete')){
+        require('Controllers/backend.php');
+        deleteOnePost($_GET['id']);
+        header('Location: index.php?action=adminPost');
+    }
+} 
