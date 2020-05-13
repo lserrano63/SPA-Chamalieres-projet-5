ac12156fc18d1d22b66303b66ce66781b9cf73bc
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-05-13 14:02:29 +0200

    new actions for panel animal admin

--- Views/router.php
+++ Views/router.php
@@ -25,7 +25,23 @@ if($action == 'spajson')
     } 
 } elseif ($action=="mentions"){
     require('Views/Public/mentions.php');
-}  elseif (isset($_SESSION['connected']) && ($_SESSION['connected'] == true)){
+} elseif ($action=="login"){
+    /*if (isset($_POST["name"]) && $_POST["name"] == $resultat['user'] ){
+        if (isset($_POST["password"]) && password_verify($_POST["password"], '' . $resultat['password'] )){			
+            $_SESSION['connected'] = true;
+            header('Location: index.php');
+        } else {
+            echo 'Mot de passe non valide';
+        }
+    }*/
+    if(!empty($_POST['name']) && !empty($_POST['password'])){
+        $password = password_verify($_POST["password"], '' . $resultat['password'] );
+        require('Controllers/frontend.php');
+        login($_POST['name'], $password);
+    } else {
+        require('Views/Public/login.php');
+    }
+} elseif (isset($_SESSION['connected']) && ($_SESSION['connected'] == true)){
     if ($action == 'disconnect') {
         $_SESSION = array();
         session_destroy();
@@ -40,6 +56,27 @@ if($action == 'spajson')
             require('Controllers/backend.php');
             addOnePost($_POST['title'], $_POST['post']);
         }
+    } elseif ($action == 'formAnimal') {
+        require('Views/Private/adminformAnimal.php');
+    } elseif ($action == 'addAnimal') {
+        if (!empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['type']) && !empty($_POST['age']) && !empty($_POST['sexe'])){
+            require('Controllers/backend.php');
+            addOneAnimal($_POST['name'], $_POST['description'], $_POST['type'], $_POST['age'], $_POST['sexe']);
+        }
+    } elseif ($action == 'adminAnimal') {
+        require('Views/Private/adminAnimalView.php');
+    } elseif ((isset($_GET['id']) && $_GET['id'] > 0) && ($action == 'adminAnimalModify')) {
+        require('Controllers/frontend.php');
+        require('Controllers/backend.php');
+        viewAnimalAdmin();
+    } elseif ((isset($_GET['id']) && $_GET['id'] > 0) && ($action == 'adminAnimalModified')){
+        require('Controllers/backend.php');
+        modifyOneAnimal($_POST['name'], $_POST['description'], $_POST['type'], $_POST['age'], $_POST['sexe'], $_GET['id']);
+        header('Location: index.php?action=adminAnimal');
+    } elseif ((isset($_GET['id']) && $_GET['id'] > 0) && ($action == 'adminAnimalDelete')){
+        require('Controllers/backend.php');
+        deleteOneAnimal($_GET['id']);
+        header('Location: index.php?action=adminAnimal');
     } elseif ($action == 'viewComments') {
         require('Views/Private/adminManageComments.php');
     } elseif ($action == 'removeComment') {
@@ -48,8 +85,9 @@ if($action == 'spajson')
             removeComment($_GET['id']);
             header('Location: index.php?action=adminManageComments');
         }
-    }
-    elseif ($action == 'adminPost') {
+    } elseif ($action == 'adminRights') {
+        require('Views/Private/adminManageRights.php');
+    } elseif ($action == 'adminPost') {
         require('Views/Private/adminPostView.php');
     } elseif ((isset($_GET['id']) && $_GET['id'] > 0) && ($action == 'adminPostModify')) {
         require('Controllers/frontend.php');
