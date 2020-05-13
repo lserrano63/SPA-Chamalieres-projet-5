cf79380b9a952cf46664c45b3f203152a0911532
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-05-13 14:00:10 +0200

    creation animal manager

--- Models/animalManager.php
+++ Models/animalManager.php
@@ -2,10 +2,57 @@
 require_once('Manager.php');
 class AnimalManager extends Manager {
 
+    public function getAnimal($animal_id)
+    {
+        $db = $this->dbConnection();
+        $req = $db->prepare('SELECT id, name, description, DATE_FORMAT(date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr, type, age, sexe FROM animals WHERE id = ?');
+        $req->execute(array($animal_id));
+        $animal = $req->fetch();
+        return $animal;
+    }
+
     public function sliderAnimals()
     {
         $db = $this->dbConnection();
         $req = $db->query('SELECT * FROM animals ORDER BY id DESC LIMIT 0,5');
         return $req;
+    }
+
+    public function addAnimal($name, $description, $type, $age, $sexe)
+    {
+        $db = $this->dbConnection();
+        $reqAnimal = $db->prepare('INSERT INTO animals(name, description, date, type, age, sexe) VALUES(?, ?, NOW()), ?, ?,?');
+        $addanAnimal = $reqAnimal->execute(array($name, $description, $type, $age, $sexe));
+        return $addanAnimal;
+    }
+
+    public function modifyAnimal($name, $description, $type, $age, $sexe, $animal_id)
+    {
+        $db = $this->dbConnection();
+        $modifyanAnimal = $db->prepare('UPDATE animals SET name = :name, description = :description, type = :type, age = :age, sexe = :sexe WHERE id= :id');
+        $modifyAnimal = $modifyanAnimal->execute(array(
+            'name' => $name,
+            'description' => $description,
+            'type' => $type,
+            'age' => $age,
+            'sexe' => $sexe,
+            'id' => $animal_id
+        ));
+    }
+
+    public function removeAnimal($animal_id)
+    {
+        $db = $this->dbConnection();
+        $removeAnAnimal = $db->prepare('DELETE FROM animals WHERE id = ?');
+        $removeAnimal = $removeAnAnimal->execute(array($animal_id));
+        return $removeAnimal;
+    }
+
+    public function getAnimals()
+    {
+        $db = $this->dbConnection();
+        $req = $db->query('SELECT id, name, description, DATE_FORMAT(date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr, type, age, sexe FROM animals ORDER BY date ');
+        $animals = $req->fetchAll();
+        return $animals;
     }
 }
