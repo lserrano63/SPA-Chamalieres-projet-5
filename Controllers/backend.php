c2dbb7892d90a4a65309f7bfaaf9c831b8fd4da2
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-05-13 13:59:51 +0200

    add somes functions for animals

--- Controllers/backend.php
+++ Controllers/backend.php
@@ -36,9 +36,52 @@ function viewPostAdmin(){
     $postManager = new PostManager();
     if (isset($_GET['id']) && $_GET['id'] > 0) {
         $postAdmin = $postManager->getPost($_GET['id']);
-        require('Views/adminModify.php');
+        require('Views/Private/adminModify.php');
     }
     else {
         echo 'Erreur : aucun identifiant de billet envoyé';
     }
 }
+
+function viewAnimalAdmin(){
+    $animalManager = new AnimalManager();
+    if (isset($_GET['id']) && $_GET['id'] > 0) {
+        $animalAdmin = $animalManager->getAnimal($_GET['id']);
+        require('Views/Private/adminAnimalModify.php');
+    }
+    else {
+        echo 'Erreur : aucun identifiant de billet envoyé';
+    }
+}
+
+function addOneAnimal($name, $description, $type, $age, $sexe)
+{
+    $animalManager = new AnimalManager();
+    $addanAnimal = $animalManager->addAnimal($name, $description, $type, $age, $sexe);
+    if ($addanAnimal === false) {
+        die('Impossible de créer la fiche animale !');
+    }
+    else {
+        header('Location: index.php');
+    }
+}
+
+function modifyOneAnimal($name, $description, $type, $age, $sexe, $animal_id)
+{
+    $animalManager = new AnimalManager();
+    $animalManager->modifyAnimal($name, $description, $type, $age, $sexe, $animal_id);
+}
+
+function deleteOneAnimal($animal_id)
+{
+    $animalManager = new AnimalManager();
+    $animalManager->removeAnimal($animal_id);
+    $commentManager = new CommentManager();
+    $commentManager->removeAllCommentsFromPost($animal_id);
+}
+
+/*function removeComment($comment_id) ANIMAL comment a faire en commentmanagaer
+{
+    $commentManager = new CommentManager();
+    $commentManager->removeComment($comment_id);
+}*/
