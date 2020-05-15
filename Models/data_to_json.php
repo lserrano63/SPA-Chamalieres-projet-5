44bdb7729089e3752d82d511a83549b400b5ae64
lserrano63 <toushalo@hotmail.fr>
2020-05-15 23:37:55 +0200

    updating names

--- Models/data_to_json.php
+++ Models/data_to_json.php
@@ -2,7 +2,7 @@
 require_once('Manager.php');
 class Data_to_json extends Manager {
 
-    public function data_to_json()
+    public function all_spas()
     {
         $db = $this->dbConnection();
         $reqjson = $db->query('SELECT * FROM others_spa');
--- Views/Private/spa_json.php
+++ Views/Private/spa_json.php
@@ -1,7 +1,7 @@
 <?php
 
 $datamanager = new Data_to_json();
-$getjson = $datamanager->data_to_json();
+$getjson = $datamanager->all_spas();
 $json_array = array();
 while  ($getTheJson = $getjson->fetch()) {
     $json_array[] = $getTheJson;
