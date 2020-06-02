3a596d67db6009ba703672fb69b3fe06c606ea8f
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-06-02 23:38:25 +0200

    functions sendemail and add image up

--- MVC/Controllers/backend.php
+++ MVC/Controllers/backend.php
@@ -78,14 +78,45 @@ class BackEndController {
 
     public function addOneAnimal($name, $description, $type, $age, $sexe)
     {
-        $animalManager = new AnimalManager();
-        /* FAIRE CHECK FILES I F*/
-        $addanAnimal = $animalManager->addAnimal($name, $description, $type, $age, $sexe);   
-        if ($addanAnimal === false) {
-            die('Impossible de créer la fiche animale !');
-        }
-        else {
-            header('Location: index.php');
+        if(isset($_FILES["animal_image"])){
+
+            $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg");
+            $filename = $_FILES["animal_image"]["name"];
+            $filetype = $_FILES["animal_image"]["type"];
+            $filesize = $_FILES["animal_image"]["size"];
+
+            $ext = pathinfo($filename, PATHINFO_EXTENSION);
+            if(!array_key_exists($ext, $allowed)){
+                die("Erreur : Veuillez sélectionner un format jpg.");
+            }
+
+            $maxsize = 5 * 1024 * 1024; //5Mo 
+            if($filesize > $maxsize) { //Checking file's weight
+                die("Error: La taille du fichier est supérieure à 5 Mo.");
+            }
+
+            if(in_array($filetype, $allowed)){ //Checking if the extension is allowed
+                if(file_exists("images/animals/" . $_FILES["animal_image"]["name"])){ //Checking if the image is already in the folder with the same name
+                    echo $_FILES["animal_image"]["name"] . " existe déjà.";
+                } else { //If not we send the form data, we change the image's location and we rename it with the last entry on our database.
+                    $animalManager = new AnimalManager();
+                    $addanAnimal = $animalManager->addAnimal($name, $description, $type, $age, $sexe);    
+                    if ($addanAnimal === false) {
+                        die('Impossible de créer la fiche animale !');
+                    }
+                    else {
+                        $lastanimal = $animalManager->lastAnimal();
+                        move_uploaded_file($_FILES["animal_image"]["tmp_name"], "images/animals/" . $_FILES["animal_image"]["name"]);
+                        rename("images/animals/" . $_FILES["animal_image"]["name"], "images/animals/" . $lastanimal['id'] . ".jpg");
+                        echo "Votre fichier a été téléchargé.";
+                        //header('Location: index.php?action=viewAnimals');
+                    }
+                } 
+            } else {
+                echo "Error: Il y a eu un problème de téléchargement. Veuillez réessayer."; 
+            }
+        } else {
+            echo "Error: " . $_FILES["animal_image"]["error"];
         }
     }
 
@@ -112,6 +143,17 @@ class BackEndController {
     {
         $animalManager = new AnimalManager();
         $animalManager->modifyAnimal($name, $description, $type, $age, $sexe, $animal_id);
+    }
+
+    public function sendEmailCreationAdmin($email)
+    {
+        $from = "spa.chamalieres.contact@gmail.com";
+        $to = $email; 
+        $subject = "Création compte administrateur sur le site de la SPA de Chamlières"; 
+        $message = 'Veuillez remplir le formulaire à l\'addresse suivante : https://projetsls.fr/SPA-Chamalieres/index.php?action=newAdminUser'; 
+        $headers = "From:" . $from;
+        mail($to,$subject,$message, $headers);
+        header('Location: index.php?action=admin');
     }
 
 }
