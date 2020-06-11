238cca7b2acc2d6b841f8ad2b1d5a451bc74be0c
lserrano63 <toushalo@hotmail.fr>
2020-06-12 01:04:01 +0200

    css change

--- MVC/Views/template.php
+++ MVC/Views/template.php
@@ -10,7 +10,7 @@
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <script src="https://cdn.tiny.cloud/1/tarq1lkuc10asmbe1nph78zrm6tf4x6ktxc9qqblqgz4bcva/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-        <link href="css/style.css" rel="stylesheet"/>
+        <link href="/SPA-Chamalieres/css/style.css" rel="stylesheet"/>
         <?= $additionnalHead ?>
         <title><?= $title ?></title>
     </head>
