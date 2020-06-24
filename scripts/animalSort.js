a702d5d32f96ba669c00a440c409adc258c5d2d8
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-06-24 14:42:23 +0200

    test Sort on a li clic

--- scripts/animalSort.js
+++ scripts/animalSort.js
@@ -10,6 +10,23 @@ class AnimalSort {
         this.snakes = document.getElementById("serpent");
         this.mouses = document.getElementById("souris");
         this.turtles = document.getElementById("tortue");
-    }   
+        this.animals = document.querySelectorAll(".animals");
+        this.animalsLength = this.animals.length;
+        this.test = document.getElementById("all_animals");
+        this.initControls();
+    } 
+
+    catsSort(){
+        this.animals[this.animalsLength].classList.add("not_visible");
+        if (!this.animals.hasAttribute("data-animal-type") && data-animal-type === "chat") {
+            this.test.innerHTML = '<p>Rien n\'a été trouvé lors de votre recherche !</p>';
+        } else {
+            this.cats.classList.remove("not_visible");
+        }
+    }
+
+    initControls(){
+        this.cats.addEventListener("click", this.catsSort.bind(this));
+    }
 }
 var animalSort = new AnimalSort();
