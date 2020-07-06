ffc679e7dd11b1d9e2457848166319c974c16049
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-07-06 04:14:08 +0200

    preload from database

--- MVC/Views/Private/adminAnimalModify.php
+++ MVC/Views/Private/adminAnimalModify.php
@@ -19,12 +19,13 @@
                         <div class="form-group">
                         <label for="type">Type d'animal :</label>
                             <select id="type" name="type">
+                                <option value="<?= $animalAdmin['type'];?>" disabled selected><?= ucfirst($animalAdmin['type']);?></option>
                                 <option value="chat">Chat</option>
                                 <option value="chien">Chien</option>
                                 <option value="hamster">Hamster</option>
                                 <option value="rat">Rat</option>
                                 <option value="lapin">Lapin</option>
-                                <option value="perroquet">perroquet</option>
+                                <option value="perroquet">Perroquet</option>
                                 <option value="poisson">Poisson</option>
                                 <option value="serpent">Serpent</option>
                                 <option value="souris">Souris</option>                                    
@@ -34,6 +35,7 @@
                         <div class="form-group">
                         <label for="age">Age :</label>
                             <select id="age" name="age">
+                                <option value="<?= $animalAdmin['age'];?>" disabled selected><?= $animalAdmin['age'];?> an(s)</option>
                                 <option value="0">0 an</option>
                                 <option value="1">1 an</option>
                                 <option value="2">2 ans</option>
@@ -61,6 +63,7 @@
                         <div class="form-group">
                         <label for="sexe">Sexe :</label>
                             <select id="sexe" name="sexe">
+                                <option value="<?= $animalAdmin['sexe'];?>" disabled selected><?= $animalAdmin['sexe'];?></option>
                                 <option value="male">Male</option>
                                 <option value="femelle">Femelle</option>
                             </select>
