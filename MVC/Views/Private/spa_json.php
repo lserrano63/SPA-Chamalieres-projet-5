a96b73fa79e18f210f2e0d0412f676eabb2ce456
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-07-07 06:42:21 +0200

    data in controller

--- MVC/Views/Private/spa_json.php
+++ MVC/Views/Private/spa_json.php
@@ -1,7 +1,5 @@
 <?php
 
-$datamanager = new \App\Models\Data_to_json();
-$getjson = $datamanager->all_spas();
 $json_array = array();
 while  ($getTheJson = $getjson->fetch()) {
     $json_array[] = $getTheJson;
