d5913961c2bbccc6b8c01207fd734a462e4f1621
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-07-07 06:43:39 +0200

    new error view / no duplicate

--- MVC/Views/Public/errorView.php
+++ MVC/Views/Public/errorView.php
@@ -3,11 +3,5 @@
 ?>
 <?php ob_start(); ?>
 
-<section id="error" class="container rounded bg-secondary pt-3 pb-1">
-    <?php
-        echo $errors;
-    ?>
-</section>
-
 <?php $content = ob_get_clean(); ?>
 <?php require('MVC/Views/template.php'); ?>
